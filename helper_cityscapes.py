8382753ee0c339d52b9035e8dee81161f76ac25e
Nikolay Falaleev <nikolasent@gmail.com>
2017-10-06 00:13:24 +0300

    Add project code and images
    feat: Add the project for Kitti and Cityscapes datasets

--- helper_cityscapes.py
+++ helper_cityscapes.py
@@ -62,8 +62,8 @@ def img_size(img):
     
 def random_crop(img, gt):
     h,w = img_size(img)
-    nw = random.randint(1150, w-5) # Random crop size
-    nh = int(nw / 3.3) # Keep original aspect ration
+    nw = random.randint(768, w-2) # Random crop size
+    nh = int(nw / 2) # Keep original aspect ration
     x1 = random.randint(0, w - nw) # Random position of crop
     y1 = random.randint(0, h - nh)
     return img[y1:(y1+nh), x1:(x1+nw), :], gt[y1:(y1+nh), x1:(x1+nw), :]
@@ -74,8 +74,27 @@ def bc_img(img, s = 1.0, m = 0.0):
     img[img > 255] = 255
     img[img < 0] = 0
     img = img.astype(np.uint8)
-    return img    
-    
+    return img   
+
+train_dataset_dir = 'data/leftImg8bit/train_ds/'
+val_dataset_dir = 'data/leftImg8bit/val_ds/'
+gt_dataset_dir = 'data/leftImg8bit/gt_ds/'
+
+road_color = np.array([128, 64, 128, 255])
+car_color = np.array([0, 0, 142, 255])
+
+img_input = []
+gt_input = []
+
+#Load data into RAM, if you have enough space
+def load_data():
+    image_paths = os.listdir(gt_dataset_dir)
+    for image_file in image_paths:
+        gt_image = scipy.misc.imread(os.path.join(gt_dataset_dir, image_file))
+        image = scipy.misc.imread(os.path.join(train_dataset_dir, image_file[:-5]+'.png'))
+        gt_input.append(gt_image)
+        img_input.append(image)
+
 def gen_batch_function(data_folder, image_shape):
     """
     Generate function to create batches of training data
@@ -89,44 +108,32 @@ def gen_batch_function(data_folder, image_shape):
         :param batch_size: Batch Size
         :return: Batches of training data
         """
-        image_paths = glob(os.path.join(data_folder, 'image_2', '*.png'))
-        label_paths = {
-            re.sub(r'_(lane|road)_', '_', os.path.basename(path)): path
-            for path in glob(os.path.join(data_folder, 'gt_image_2', '*_road_*.png'))}
-        background_color = np.array([255, 0, 0])
-        sideroad_color = np.array([0, 0, 0])
-        road_color = np.array([255, 0, 255])
-
+        
+        image_paths = os.listdir(gt_dataset_dir)
         random.shuffle(image_paths)
         for batch_i in range(0, len(image_paths), batch_size):
             images = []
             gt_images = []
             for image_file in image_paths[batch_i:batch_i+batch_size]:
-                gt_image_file = label_paths[os.path.basename(image_file)]
-                image = scipy.misc.imread(image_file)
-                gt_image = scipy.misc.imread(gt_image_file)
-                #image, gt_image = random_crop(image, gt_image) #Random crop augmentation
+                gt_image = scipy.misc.imread(os.path.join(gt_dataset_dir, image_file))
+                image = scipy.misc.imread(os.path.join(train_dataset_dir, image_file[:-5]+'.png'))
+                image, gt_image = random_crop(image, gt_image) #Random crop augmentation
                 image = scipy.misc.imresize(image, image_shape)
-                contr = random.uniform(0.85, 1.15) # Contrast augmentation
-                bright = random.randint(-45, 30) # Brightness augmentation
-                image = bc_img(image, contr, bright)
                 gt_image = scipy.misc.imresize(gt_image, image_shape)
-                gt_bg = np.all(gt_image == background_color, axis=2)
+                contr = random.uniform(0.85, 1.15) # Contrast augmentation
+                bright = random.randint(-40, 30) # Brightness augmentation
+                image = bc_img(image, contr, bright)
+                gt_road = np.all(gt_image == road_color, axis=2)
+                gt_road = gt_road.reshape(*gt_road.shape, 1)
+                gt_car = np.all(gt_image == car_color, axis=2)
+                gt_car = gt_car.reshape(*gt_car.shape, 1)
+                gt_obj = np.concatenate((gt_road, gt_car), axis=2)
+                gt_bg = np.all(gt_obj == 0, axis=2)
                 gt_bg = gt_bg.reshape(*gt_bg.shape, 1)
-                gt_image = np.concatenate((gt_bg, np.invert(gt_bg)), axis=2)
-                #gt_sr = np.all(gt_image == sideroad_color, axis=2)
-                #gt_sr = gt_sr.reshape(*gt_sr.shape, 1)
-                #gt_r = np.all(gt_image == road_color, axis=2)
-                #gt_r = gt_r.reshape(*gt_r.shape, 1)
-                
-                #gt_image = np.concatenate((gt_bg, gt_r, gt_sr), axis=2) 
                 images.append(image)
                 gt_images.append(gt_image)
-
             yield np.array(images), np.array(gt_images)
     return get_batches_fn
-    
-
 
 def denoise_img(img):
     eroded_img = ndimage.binary_erosion(img)
@@ -143,28 +150,31 @@ def gen_test_output(sess, logits, keep_prob, image_pl, data_folder, image_shape,
     :param image_shape: Tuple - Shape of image
     :return: Output for for each test image
     """
-    for image_file in glob(os.path.join(data_folder, 'image_2', '*.png')):
-        image = scipy.misc.imresize(scipy.misc.imread(image_file), image_shape)
-
+    for image_file in os.listdir(val_dataset_dir):
+        image = scipy.misc.imresize(scipy.misc.imread(os.path.join(val_dataset_dir, image_file)), image_shape)
+        street_im = scipy.misc.toimage(image)
         im_softmax = sess.run(
             [tf.nn.softmax(logits)],
             {keep_prob: 1.0, image_pl: [image]})
+        # Road
+        im_softmax_r = im_softmax[0][:, 0].reshape(image_shape[0], image_shape[1])
+        segmentation_r = (im_softmax_r > 0.5).reshape(image_shape[0], image_shape[1], 1)
+        mask = np.dot(segmentation_r, np.array([[128, 64, 128, 64]]))
+        mask = scipy.misc.toimage(mask, mode="RGBA")
+        street_im.paste(mask, box=None, mask=mask)
+        # Car
         im_softmax_r = im_softmax[0][:, 1].reshape(image_shape[0], image_shape[1])
         segmentation_r = (im_softmax_r > 0.5).reshape(image_shape[0], image_shape[1], 1)
-        #segmentation_r = denoise_img(segmentation_r)
-        mask = np.dot(segmentation_r, np.array([[0, 255, 0, 127]]))
-        if num_classes == 3:
-            im_softmax_sr = im_softmax[0][:, 2].reshape(image_shape[0], image_shape[1])
-            segmentation_sr = (im_softmax_sr > 0.5).reshape(image_shape[0], image_shape[1], 1)
-            #segmentation_sr = denoise_img(segmentation_sr)
-            mask = mask + np.dot(segmentation_sr, np.array([[0, 0, 255, 127]]))
+        mask = np.dot(segmentation_r, np.array([[0, 0, 142, 64]]))
         mask = scipy.misc.toimage(mask, mode="RGBA")
-        street_im = scipy.misc.toimage(image)
         street_im.paste(mask, box=None, mask=mask)
-        mask = scipy.misc.toimage(mask)
+        # Signs
+        #im_softmax_r = im_softmax[0][:, 3].reshape(image_shape[0], image_shape[1])
+        #segmentation_r = (im_softmax_r > 0.5).reshape(image_shape[0], image_shape[1], 1)
+        #mask += np.dot(segmentation_r, np.array([[220, 220, 0, 127]]))
+        
         yield os.path.basename(image_file), np.array(street_im)
 
-        
 
 def save_inference_samples(runs_dir, data_dir, sess, image_shape, logits, keep_prob, input_image, num_classes):
     # Make folder for current run
